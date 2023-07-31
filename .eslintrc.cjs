65020d4670086a209f46018130c946ff267f799f
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-07-31 13:30:43 +0300

    turn off some rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,10 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
   },
 };
 
