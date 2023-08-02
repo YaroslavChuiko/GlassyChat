9891961963461badd1fdd96c46623fdfe3bc4841
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-08-02 15:21:44 +0300

    disable @typescript-eslint/consistent-type-definitions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,6 +23,7 @@ const config = {
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
   },
 };
 
