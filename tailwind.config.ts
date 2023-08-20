dada7a421226e8d231ada91cb3ff4a76cc0e0969
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-08-20 14:20:17 +0300

    replace windy-radix-palette to tailwindcss-radix-colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#fff",
+        black: "#000",
+      },
+    },
   },
-  plugins: [require("windy-radix-palette")],
+  plugins: [require("tailwindcss-radix-colors")], // https://www.radix-ui.com/colors/docs/palette-composition/scales
 } satisfies Config;
