af5fad294d0093cad1973183db4049b17f039d9c
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-08-23 20:30:21 +0300

    can control colorTheme and darkMode

--- src/components/Sidebar/DropdownMainMenu.tsx
+++ src/components/Sidebar/DropdownMainMenu.tsx
@@ -1,6 +1,4 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import React from "react";
-import IconButton from "../IconButton";
 import {
   BlendingModeIcon,
   ChevronRightIcon,
@@ -10,13 +8,27 @@ import {
   MoonIcon,
   PlusIcon,
 } from "@radix-ui/react-icons";
+import * as Switch from "@radix-ui/react-switch";
+import { useAppStore } from "~/store/store";
+import { isColorTheme } from "~/types/ColorTheme";
+import IconButton from "../IconButton";
 
 type Props = {
   className?: string;
 };
 
 export default function DropdownMainMenu({ className }: Props) {
-  const [person, setPerson] = React.useState("pedro");
+  const { colorTheme, setColorTheme, darkMode, toggleDarkMode } = useAppStore();
+
+  const preventClosingMenu = (e: Event) => {
+    e.preventDefault();
+  };
+
+  const handleThemeChange = (value: string) => {
+    if (isColorTheme(value)) {
+      setColorTheme(value);
+    }
+  };
 
   return (
     <DropdownMenu.Root>
@@ -33,17 +45,29 @@ export default function DropdownMainMenu({ className }: Props) {
           sideOffset={10}
           align="start"
         >
-          <DropdownMenu.Item className="my-[2px] flex h-[30px] select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11">
+          <DropdownMenu.Item className="my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none transition active:scale-[0.98] data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11 ">
             <PlusIcon height={20} width={20} className="ml-2 mr-5" />
             New Chat
           </DropdownMenu.Item>
-          <DropdownMenu.Item className="my-[2px] flex h-[30px] select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11">
+          <DropdownMenu.CheckboxItem
+            className="my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none transition active:scale-[0.98] data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11"
+            checked={darkMode}
+            onCheckedChange={toggleDarkMode}
+            onSelect={preventClosingMenu}
+          >
             <MoonIcon height={20} width={20} className="ml-2 mr-5" />
             Night Mode
-          </DropdownMenu.Item>
+            <Switch.Root
+              className="relative ml-auto  h-[16px] w-[30px] cursor-pointer select-none rounded-full bg-graya-9 outline-none data-[state=checked]:bg-graya-12"
+              id="airplane-mode"
+              checked={darkMode}
+            >
+              <Switch.Thumb className="block h-[12px] w-[12px] translate-x-0.5 rounded-full bg-white transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[14px]" />
+            </Switch.Root>
+          </DropdownMenu.CheckboxItem>
 
           <DropdownMenu.Sub>
-            <DropdownMenu.SubTrigger className="group relative my-[2px] flex h-[30px] select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[highlighted]:data-[state=open]:bg-graya-5 data-[state=open]:bg-graya-5 data-[disabled]:text-gray-11">
+            <DropdownMenu.SubTrigger className="group relative my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none transition data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[highlighted]:data-[state=open]:bg-graya-5 data-[state=open]:bg-graya-5 data-[disabled]:text-gray-11">
               <BlendingModeIcon height={20} width={20} className="ml-2 mr-5" />
               Theme
               <div className="ml-auto pl-[20px]">
@@ -53,66 +77,55 @@ export default function DropdownMainMenu({ className }: Props) {
 
             <DropdownMenu.Portal>
               <DropdownMenu.SubContent
-                className="min-w-[220px] rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
+                className="min-w-[200px] rounded-lg bg-[rgba(255,_255,_255,0.6)] p-1 text-sm font-medium shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] backdrop-blur will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade  data-[side=top]:animate-slideDownAndFade"
                 sideOffset={2}
                 alignOffset={-5}
               >
                 <DropdownMenu.RadioGroup
-                  value={person}
-                  onValueChange={setPerson}
+                  value={colorTheme}
+                  onValueChange={handleThemeChange}
                 >
                   <DropdownMenu.RadioItem
-                    className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 relative flex h-[30px] select-none items-center rounded-[3px] px-[5px] pl-[25px] leading-none outline-none data-[disabled]:pointer-events-none"
-                    value="pedro"
+                    className="my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 pl-[25px] leading-none text-gray-12 outline-none transition active:scale-[0.98] data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11"
+                    value="blue"
+                    onSelect={preventClosingMenu}
                   >
                     <DropdownMenu.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
                       <DotFilledIcon />
                     </DropdownMenu.ItemIndicator>
-                    Red
+                    Blue
+                    <div className="ml-auto h-5 w-8 rounded-md bg-gradient-to-br from-[#6366f1] to-[#d946ef] pl-[20px]"></div>
                   </DropdownMenu.RadioItem>
                   <DropdownMenu.RadioItem
-                    className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 relative flex h-[30px] select-none items-center rounded-[3px] px-[5px] pl-[25px] leading-none outline-none data-[disabled]:pointer-events-none"
-                    value="colm"
+                    className="my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 pl-[25px] leading-none text-gray-12 outline-none transition active:scale-[0.98] data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11"
+                    value="green"
+                    onSelect={preventClosingMenu}
                   >
                     <DropdownMenu.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
                       <DotFilledIcon />
                     </DropdownMenu.ItemIndicator>
                     Green
+                    <div className="ml-auto h-5 w-8 rounded-md bg-gradient-to-br from-[#4ADE80] to-[#06B6D4] pl-[20px]"></div>
+                  </DropdownMenu.RadioItem>
+                  <DropdownMenu.RadioItem
+                    className="my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 pl-[25px] leading-none text-gray-12 outline-none transition active:scale-[0.98] data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11"
+                    value="yellow"
+                    onSelect={preventClosingMenu}
+                  >
+                    <DropdownMenu.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
+                      <DotFilledIcon />
+                    </DropdownMenu.ItemIndicator>
+                    Yellow
+                    <div className="ml-auto h-5 w-8 rounded-md bg-gradient-to-br from-[#FCD34D] to-[#F97316] pl-[20px]"></div>
                   </DropdownMenu.RadioItem>
                 </DropdownMenu.RadioGroup>
               </DropdownMenu.SubContent>
             </DropdownMenu.Portal>
           </DropdownMenu.Sub>
 
-          {/* <DropdownMenu.Separator className="m-[5px] h-[1px] bg-gray-9" />
-
-          <DropdownMenu.Label className="text-mauve11 leading-[25p h-[30px]x] pl-[25px] text-sm">
-            Theme
-          </DropdownMenu.Label>
-          <DropdownMenu.RadioGroup value={person} onValueChange={setPerson}>
-            <DropdownMenu.RadioItem
-              className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 relative flex h-[25px] h-[30px] select-none items-center rounded-[3px] px-[5px] pl-[25px] leading-none outline-none data-[disabled]:pointer-events-none"
-              value="pedro"
-            >
-              <DropdownMenu.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
-                <DotFilledIcon />
-              </DropdownMenu.ItemIndicator>
-              Pedro Duarte
-            </DropdownMenu.RadioItem>
-            <DropdownMenu.RadioItem
-              className="text-violet11 data-[disabled]:text-mauve8 data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 relative flex h-[25px] h-[30px] select-none items-center rounded-[3px] px-[5px] pl-[25px] leading-none outline-none data-[disabled]:pointer-events-none"
-              value="colm"
-            >
-              <DropdownMenu.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
-                <DotFilledIcon />
-              </DropdownMenu.ItemIndicator>
-              Colm Tuite
-            </DropdownMenu.RadioItem>
-          </DropdownMenu.RadioGroup> */}
-
           <DropdownMenu.Separator className="m-[5px] h-[1px] bg-gray-9" />
 
-          <DropdownMenu.Item className="my-[2px] flex h-[30px] select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11">
+          <DropdownMenu.Item className="my-[2px] flex h-[30px] cursor-pointer select-none items-center rounded-[5px] p-1 leading-none text-gray-12 outline-none transition active:scale-[0.98] data-[disabled]:pointer-events-none data-[highlighted]:bg-graya-4 data-[disabled]:text-gray-11">
             <ExitIcon height={20} width={20} className="ml-2 mr-5" />
             Log Out
           </DropdownMenu.Item>
