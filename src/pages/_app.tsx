06cf5a2662b5c6de97a0da6adf78e0a6b572e9fd
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-08-23 20:30:53 +0300

    apply dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import PusherProvider from "~/provider/PusherProvider";
+import { useEffect } from "react";
+import { useAppStore } from "~/store/store";
 // import { Lato } from "next/font/google";
 
 // const lato = Lato({
@@ -15,6 +17,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const applyDarkMode = useAppStore((state) => state.applyDarkMode);
+
+  useEffect(() => {
+    applyDarkMode();
+  }, [applyDarkMode]);
+
   return (
     <SessionProvider session={session}>
       <PusherProvider>
