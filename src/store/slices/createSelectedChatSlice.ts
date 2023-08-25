74866a4da2be0082370bf65196aab559396841fb
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-08-25 20:02:33 +0300

    create SelectedChatSlice

---
+++ src/store/slices/createSelectedChatSlice.ts
@@ -0,0 +1,14 @@
+import { type StateCreator } from "zustand";
+import { type Chat } from "~/types/Chat";
+
+export type SelectedChatSlice = {
+  selectedChat: Chat | null;
+  setSelectedChat: (chat: Chat) => void;
+};
+
+export const createSelectedChatSlice: StateCreator<SelectedChatSlice> = (
+  set
+) => ({
+  selectedChat: null,
+  setSelectedChat: (chat) => set({ selectedChat: chat }),
+});
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,28 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 import { createThemeSlice, type ThemeSlice } from "./slices/createThemeSlice";
+import {
+  createSelectedChatSlice,
+  type SelectedChatSlice,
+} from "./slices/createSelectedChatSlice";
 
-type StoreState = ThemeSlice;
+type StoreState = ThemeSlice & SelectedChatSlice;
 
 export const useAppStore = create<StoreState>()(
   persist(
     (...a) => ({
       ...createThemeSlice(...a),
+      ...createSelectedChatSlice(...a),
     }),
     {
       name: "theme-storage", // name of the item in the storage (must be unique)
       storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+      partialize: (state) =>
+        Object.fromEntries(
+          Object.entries(state).filter(([key]) =>
+            ["colorTheme", "darkMode"].includes(key)
+          )
+        ),
     }
   )
 );
