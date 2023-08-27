bfa8a12be84c1e5541b314d0fa3c31ce3cb913df
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-08-27 13:02:35 +0300

    refactor: create new components

--- src/components/Chat/Header.tsx
+++ src/components/Chat/Header.tsx
@@ -1,35 +1,18 @@
 import { type Chat } from "~/types/Chat";
-import { api } from "~/utils/api";
-import Avatar from "../Avatar";
+import ChatActionsDropdown from "./ChatActionsDropdown";
+import ChatInfo from "./ChatInfo";
 
 type Props = {
-  chat: Chat;
+  selectedChat: Chat;
 };
 
-export default function Header({ chat }: Props) {
-  const { id, name, color } = chat;
-
-  const membersCount = api.room.getMembersCount.useQuery({ id });
+export default function Header({ selectedChat }: Props) {
+  const { userRole, type } = selectedChat;
 
   return (
     <div className="flex h-20 w-full items-center justify-between bg-graya-3 px-[30px] py-[15px] transition-colors dark:bg-graydarka-3">
-      <div className="flex items-center">
-        <Avatar name={name} color={color} size="lg" className="mr-3" />
-        <div className="flex-1 overflow-hidden">
-          <div className=" flex items-center justify-between">
-            <div className="flex-1 truncate text-base font-semibold text-gray-12 transition-colors dark:text-graydark-12">
-              {name}
-            </div>
-          </div>
-          <div className="truncate text-sm text-gray-11 transition-colors dark:text-graydark-11">
-            {membersCount.isSuccess
-              ? `${membersCount.data?.count} ${
-                  membersCount.data?.count > 1 ? "members" : "member"
-                }`
-              : "Loading..."}
-          </div>
-        </div>
-      </div>
+      <ChatInfo chat={selectedChat} />
+      <ChatActionsDropdown userRole={userRole} chatType={type} />
     </div>
   );
 }
--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -10,7 +10,7 @@ export default function Chat({ className, ...props }: ComponentProps<"div">) {
       className={`overflow-hidden rounded-3xl bg-[rgba(255,_255,_255,_0.8)] shadow-[0px_4px_24px_-1px_rgba(0,_0,_0,_0.2)] backdrop-blur transition-colors dark:bg-[rgba(0,_0,_0,_0.8)] ${className}`}
       {...props}
     >
-      {selectedChat && <Header chat={selectedChat} />}
+      {selectedChat && <Header selectedChat={selectedChat} />}
     </div>
   );
 }
