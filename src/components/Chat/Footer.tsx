874a00ee62ddf0621aaf71f7075704f5db6380f4
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-09-04 10:11:19 +0300

    update send messages form

--- src/components/Chat/Footer.tsx
+++ src/components/Chat/Footer.tsx
@@ -1,43 +1,84 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { PaperPlaneIcon } from "@radix-ui/react-icons";
-import React, { useState } from "react";
-import IconButton from "../IconButton";
+import React from "react";
+import { useForm } from "react-hook-form";
 import { api } from "~/utils/api";
+import IconButton from "../IconButton";
 
 type Props = {
   chatId: string;
 };
 
+type FormValues = {
+  message: string;
+};
+
+const defaultValues: FormValues = {
+  message: "",
+};
+
 export default function Footer({ chatId }: Props) {
-  const [text, setText] = useState("");
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
-    //another way to auto resize the textarea https://medium.com/@oherterich/creating-a-textarea-with-dynamic-height-using-react-and-typescript-5ed2d78d9848
-
-    e.target.style.height = "55px";
-    e.target.style.height = e.target.scrollHeight + "px";
-  };
+  const {
+    handleSubmit,
+    register,
+    formState: { isDirty, isSubmitting },
+    reset,
+    setValue,
+    getValues,
+  } = useForm<FormValues>({
+    defaultValues,
+  });
 
   const sendMessage = api.room.sendMessage.useMutation();
-  const handleSendMessage = () => {
-    sendMessage.mutate({ chatId, content: text });
+
+  const onSubmit = (value: FormValues) => {
+    const message = value.message.trim();
+
+    if (!isDirty || isSubmitting || !message) return;
+
+    sendMessage.mutate({ chatId, content: message });
+    reset(defaultValues);
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    autoResizeTextarea(e.target);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      if (e.ctrlKey) {
+        setValue("message", getValues("message") + "\n");
+        autoResizeTextarea(e.currentTarget);
+      } else {
+        handleSubmit(onSubmit)().catch(console.error);
+      }
+    }
+  };
+
+  const autoResizeTextarea = (target: EventTarget & HTMLTextAreaElement) => {
+    target.style.height = "55px"; // textarea default height
+    target.style.height = target.scrollHeight + "px";
   };
 
   return (
-    <div className="mx-auto my-5 flex w-3/5 items-end">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mx-auto my-5 flex w-3/5 items-end"
+    >
       <textarea
         className="mr-[15px] h-[55px] max-h-[200px] flex-1 resize-none rounded-[10px] bg-graya-3 p-[15px] text-gray-12 placeholder-gray-11 transition hover:bg-graya-4 dark:bg-graydarka-3 dark:text-graydark-12 dark:placeholder-graydark-11 dark:hover:bg-graydarka-4"
-        name="message"
         id="message"
         placeholder="Send message..."
-        value={text}
-        onChange={handleChange}
+        onInput={handleInput}
+        onKeyDown={onKeyDown}
+        {...register("message")}
       ></textarea>
       <IconButton
         className="h-[55px] w-[55px]"
         icon={<PaperPlaneIcon width={25} height={25} />}
-        onClick={handleSendMessage}
       />
-    </div>
+    </form>
   );
 }
