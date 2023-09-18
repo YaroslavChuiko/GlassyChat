c136df370f2e09c85d1c5882392d21fed366e64a
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-09-18 10:10:40 +0300

    add SearchInput

--- src/components/Sidebar/Header.tsx
+++ src/components/Sidebar/Header.tsx
@@ -1,14 +1,11 @@
 import DropdownMainMenu from "./DropdownMainMenu";
+import SearchInput from "./SearchInput";
 
 export default function Header() {
   return (
     <div className="mb-4 flex w-full">
       <DropdownMainMenu className="mr-3" />
-      <input
-        className="h-10 flex-1 rounded-lg bg-graya-3 dark:bg-graydarka-3"
-        type="text"
-        placeholder="Search"
-      />
+      <SearchInput />
     </div>
   );
 }
