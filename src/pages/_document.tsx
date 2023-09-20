8ded226f915a127642a90a2b55502c1c62d7dd87
YaroslavChuiko <32570823+JARIK0803@users.noreply.github.com>
2023-09-20 21:38:50 +0300

    add description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <meta name="theme-color" content="#fff" />
+        <meta
+          name="description"
+          content="Glassy chat is a realtime messaging app."
+        />
       </Head>
       <body>
         <Main />
